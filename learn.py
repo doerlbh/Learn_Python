3bb2c538f98192005cfa6550fbe58eeb33b4136f
doerlbh <doerlbh@gmail.com>
2016-06-22 07:36:01 -0700

    update learn.py

--- learn.py
+++ learn.py
@@ -42,3 +42,50 @@ numbers[3] = 77
 numbers
 del numbers[3]
 
+# 13 - Slicing Lists
+
+example = list('easyhoss')
+example
+
+example[4:]= list('baby')
+example
+
+example[4:]= list('racecars')
+example
+
+example = [7,8,9]
+example
+
+example[1:1] = [3,3,3]
+example
+
+example[1:5] = []
+example
+
+# 14 - Intro to Methods
+
+face = [21,18,30]
+face
+face.append
+face
+
+apples = ['i', 'love', 'apples', 'apples', 'now']
+apples
+
+apples.count('apples')
+apples.count('bow')
+
+one = [1,2,3]
+one
+
+two = [4,5,6]
+
+two
+
+one.extend(two)
+
+# 15 - More Method
+
+
+raw_input("Press<enter>")
+
