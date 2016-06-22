216ff00dcc40da8af3a675a12dce0c9f583b0710
doerlbh <doerlbh@gmail.com>
2016-06-22 07:26:15 -0700

    update learn

---
+++ learn.py
@@ -0,0 +1,44 @@
+
+
+#10 - Slicing
+
+example = [0,1,2,3,4,5,6,7,8,9]
+
+example[4:8]
+example[-5:-1]
+example[-5:]
+example[:]
+example[1:8:2]
+example[10:0:-2]
+example[::-2]
+
+#11 - Editing Sequences
+
+[7,4,5]+[4,6,5]
+'bucky' + 'roberts'
+'bucky'*10
+21*10
+[21]*10
+
+name = 'roastbeef'
+'z' in name
+'r' in name
+
+family = ['mom', 'dad', 'bro']
+'sis' in family
+'mom' in family
+
+# 12 - More List Functions
+
+numbers = [8,1,4,17,28,165,7]
+len(numbers)
+max(numbers)
+min(numbers)
+
+list('bucky')
+
+numbers
+numbers[3] = 77
+numbers
+del numbers[3]
+
